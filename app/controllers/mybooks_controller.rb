6fb792719c9d90ce2c39b54b3f8b444e454febcd
Norbert_Muzolf <nmugol000@gmail.com>
2022-02-04 08:03:50 +0000

    dodanie tego co już mam zrobone z zadania z 03.02.2022

---
+++ app/controllers/mybooks_controller.rb
@@ -0,0 +1,9 @@
+class MybooksController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "main_page#index"
+
+  get "mojastrona", to: "main_page#mojastrona"
+  get "kontakt", to: "main_page#kontakt"
+  get "books", to: "mybooks#home"
+  get "poter", to: "mybooks#poter"
+  get "akademia", to: "mybooks#akademia"
+  get "holmes", to: "mybooks#holmes"
+  get "tadeusz", to: "mybooks#tadeusz"
+  get "miasto", to: "mybooks#miasto"
+  get "ksiaze", to: "mybooks#ksiaze"
 end
